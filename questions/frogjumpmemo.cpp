359d45d4e1eee0a29bcf450f454a7aad3e732636
Ayush Mehta <ayushmehtabps@gmail.com>
2025-08-27 04:39:05 +0530

    tabulation form

--- questions/frogjumpmemo.cpp
+++ questions/frogjumpmemo.cpp
@@ -11,6 +11,24 @@ int f(int ind,vector<int>&heights,vector<int>&dp){
     return dp[ind]=min(left,right);
 }
 
+// by tabulation method
+
+int frogJump(int n , vector<int>&heights){
+    int prev=0,prev1=0;
+    for(int i=1;i<n;i++){
+        int fs=prev+abs(heights[i]-heights[i-1]);
+        int ss=INT_MAX;
+        if(i>1) ss=prev1+abs(heights[i]-heights[i-2]);
+
+        int curri=min(fs,ss);
+        prev1=prev;
+        prev=curri;
+    }
+    return prev;
+
+}
+
+
 
 int main(){
     vector<int>heights={10,20,30,10};
@@ -19,5 +37,8 @@ int main(){
 
     int ans=f(n-1,heights,dp);
     cout<<" "<<ans;
+
+    int tab=frogJump(n,heights);
+    cout<<" "<<tab;
 
 }

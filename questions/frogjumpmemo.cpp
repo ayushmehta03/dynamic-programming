d943b6be34f0d83d69860d6938e5507ff47ea811
Ayush Mehta <ayushmehtabps@gmail.com>
2025-08-27 04:09:23 +0530

    frog jum memoziation

---
+++ questions/frogjumpmemo.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int f(int ind,vector<int>&heights,vector<int>&dp){
+    if(ind==0) return 0;
+    if(dp[ind]!=-1) return dp[ind];
+    int left=f(ind-1,heights,dp)+abs(heights[ind]-heights[ind-1]);
+    int right=INT_MAX;
+    if(ind>1) right=f(ind-2,heights,dp)+abs(heights[ind]-heights[ind-2]);
+    return dp[ind]=min(left,right);
+}
+
+
+int main(){
+    vector<int>heights={10,20,30,10};
+    int n=heights.size();
+    vector<int>dp(n+1,-1);
+
+    int ans=f(n-1,heights,dp);
+    cout<<" "<<ans;
+
+}

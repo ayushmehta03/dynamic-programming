dda11716df7bad1851cd111fe46288fe63413586
Ayush Mehta <ayushmehtabps@gmail.com>
2025-08-31 17:05:43 +0530

    core recurssion unique paths

---
+++ 2D Dp/ninjatraining/uniquepaths/corerecursion.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int f(int i,int j){
+    if(i==0&&j==0) return 1;
+    if(i<0 || j<0) return 0;
+
+    int up=f(i-1,j);
+    int left=f(i,j-1);
+    return up+left;
+
+}
+
+int main(){
+    int m=3;
+    int n=7;
+    int ans=f(m-1,n-1);
+    cout<<" "<<ans;
+    
+
+}
+

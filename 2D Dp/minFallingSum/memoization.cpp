a5d4d0af754382682ff3645d7fd6c58f89ea9a2c
Ayush Mehta <ayushmehtabps@gmail.com>
2025-09-02 16:43:42 +0530

    memoization

---
+++ 2D Dp/minFallingSum/memoization.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+int f(int i,int j,vector<vector<int>>&matrix,vector<vector<int>>&dp){
+    if(j<0 || j>=matrix[0].size()) return 1e8;
+    if(i==0) return matrix[0][j];
+    if(dp[i][j]!=-1) return dp[i][j];
+    int up=matrix[i][j]+f(i-1,j,matrix,dp);
+
+    int ld=matrix[i][j]+f(i-1,j-1,matrix,dp);
+    int rd=matrix[i][j]+f(i-1,j+1,matrix,dp);
+
+    return dp[i][j]= min(up,min(ld,rd));
+}
+
+
+
+int minFallingSum(vector<vector<int>>&matrix){
+        int n=matrix.size();
+    int m=matrix[0].size();
+    int mini=1e8;
+    vector<vector<int>>dp(n,vector<int>(m,-1));
+    for(int j=0;j<m;j++){
+    mini=min(mini,f(n-1,j,matrix,dp));
+    }
+      return mini;
+
+}
+
+int main(){
+    vector<vector<int>>matrix={
+        {2,1,3},{6,5,4},{7,8,9}
+    };
+
+    int ans=minFallingSum(matrix);
+    cout<<"The minimu falling sum will be :"<<" "<<ans;
+    return 0;
+}

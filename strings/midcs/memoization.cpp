9e6e5bf5a82cb80d8443cbaa6ce14f4a763bc807
Ayush Mehta <ayushmehtabps@gmail.com>
2025-09-12 23:07:59 +0530

    memoization optimise

---
+++ strings/midcs/memoization.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+
+int f(int i,int j,string &s,string &t,vector<vector<int>>&dp){
+    if(i<0||j<0) return 0;
+
+    if(s[i]==t[j]) return 1+f(i-1,j-1,s,t,dp);
+
+
+    return dp[i][j]= max(f(i-1,j,s,t,dp),f(i,j-1,s,t,dp));
+
+
+}
+
+
+
+
+int main(){
+    string s="leetcode";
+    string t="etco";
+
+    int n=s.size();
+    int m=t.size();
+
+    vector<vector<int>>dp(n,vector<int>(m,-1));
+
+    int lcs=f(n-1,m-1,s,t,dp);
+
+    // total no of operations including deletion and insertion
+    int ans=(m+n)-2*lcs;
+    cout<<"No of opertaions needed to convert string 1 to string 2 is:"<<" "<<ans;
+    return 0;
+}

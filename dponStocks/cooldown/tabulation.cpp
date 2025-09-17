111dc6618cc77b57b789430e32009b938bc008a8
Ayush Mehta <ayushmehtabps@gmail.com>
2025-09-18 02:22:49 +0530

    tabulation

---
+++ dponStocks/cooldown/tabulation.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+
+
+int dpStocks2(vector<int>&prices){
+    int n=prices.size();
+
+    vector<vector<int>>dp(n+2,vector<int>(2,0));
+
+    dp[n][0]=dp[n][1]=0;
+
+    int profit=0;
+
+    for(int idx=n-1;idx>=0;idx--){
+
+        for(int buy=0;buy<=1;buy++){
+            if(buy==0){
+            profit=max(0+dp[idx+1][0],-prices[idx]+dp[idx+1][1]);
+            }
+
+            if(buy==1){
+                profit=max(0+dp[idx+2][1],prices[idx]+dp[idx+1][0]);
+
+
+            }   
+            dp[idx][buy]=profit;
+        }
+
+    }
+    return dp[0][0];
+
+}
+
+int main(){
+  vector<int>prices={1,2,3,0,2};
+
+  int ans=dpStocks2(prices);
+
+  cout<<"The max profit will be:"<<" "<<ans;
+
+
+
+
+}
+
+

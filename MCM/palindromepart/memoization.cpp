c462f729385dd27b1e449f5daee767beced3c7d1
Ayush Mehta <ayushmehtabps@gmail.com>
2025-09-26 16:01:47 +0530

    memoization

---
+++ MCM/palindromepart/memoization.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<climits>
+using namespace std;
+
+bool isPalindrome(int i,int j,string &s){
+    while(i<j){
+        if(s[i]!=s[j]) return false;
+        i++;
+        j--;
+    }
+    return true;
+}
+
+int f(int i,int n,string &s,vector<int>&dp){
+    if(i==n) return 0;
+    if(dp[i]!=-1) return dp[i];
+    int minCost=INT_MAX;
+    int cost=0;
+    for(int j=i;j<n;j++){
+        if(isPalindrome(i,j,s)) cost=1+f(j+1,n,s,dp);
+        minCost=min(minCost,cost);
+    }
+    return dp[i]= minCost;
+}
+
+
+
+int palindromePartition(string &s){
+    int n=s.size();
+    vector<int>dp(n,-1);
+    return f(0,n,s,dp)-1;
+
+}
+
+
+
+int main(){
+     string s="aabb";
+     int ans=palindromePartition(s);
+     cout<<"The min cuts needded for palindorme partioning is: "<<ans;
+     return 0;
+
+}
